8b26ec46a32184af2c256ec7ba380bfd95cf932b
Oleksandr Kovaliv <oleksandr.p.kovaliv@globallogic.com>
2024-12-24 11:24:56 +0200

    dotnet-js issue example

---
+++ BlazorBookReviews/wwwroot/scripts/interop.js
@@ -0,0 +1,5 @@
+﻿function initialize() {
+    let body = document.getElementsByTagName("body")[0];
+
+    body.addEventListener("mouseleave", () => DotNet.invokeMethodAsync("BlazorBookReviews", "MouseLeave", '8a9cf939-a5c0-4958-8bcf-cc6ae5ce58f6', 'Yep'));
+}
